5018ad620c0c42b7cf3742a22adbe73656d26d51
Trevor Abell <trevor@keikocode.com>
2013-08-04 21:10:04 -0700

    Added stuff

--- lisp/ekeys.el
+++ lisp/ekeys.el
@@ -1,3 +1,9 @@
 (global-set-key (kbd "C-X C-m") 'execute-extended-command)
 (global-set-key (kbd "<f11>") 'revert-buffer)
 
+;; Super + uppercase letter signifies a buffer/file
+(global-set-key (kbd "s-S")                       ;; scratch
+                (lambda()(interactive)(switch-to-buffer "*scratch*")))
+(global-set-key (kbd "s-E")                       ;; .emacs
+                (lambda()(interactive)(find-file "~/.emacs")))
+
