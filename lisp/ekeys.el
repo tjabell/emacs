df86768ed85d2b688e19c0e873c9d9e1bed32f11
Trevor Abell <trevor@keikocode.com>
2013-08-10 21:55:21 -0700

    added configuration for yas-snippets, e2wm

--- lisp/ekeys.el
+++ lisp/ekeys.el
@@ -1,3 +1,6 @@
+(global-set-key (kbd "C-x o") 'open-line)
+(global-set-key (kbd "C-o") 'other-window)
+(global-set-key (kbd "C-1") 'delete-other-windows)
 (global-set-key (kbd "C-X C-m") 'execute-extended-command)
 (global-set-key (kbd "<f11>") 'revert-buffer)
 
@@ -8,4 +11,10 @@
                 (lambda()(interactive)(find-file "~/.emacs")))
 (global-set-key (kbd "s-H")                       ;; scratch
                 (lambda()(interactive)(switch-to-buffer "*shell*")))
+
+(when (require 'e2wm nil t)
+	(progn
+		(global-set-key (kbd "s-d") 'e2wm:dp-edbi)
+		(global-set-key (kbd "s-c") 'e2wm:dp-code)))
+
 
