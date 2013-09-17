a06e665c4bb603ee850c7ad7086fc1daab82514f
Trevor Abell <trevor@keikocode.com>
2013-09-16 21:36:17 -0700

    added shortcuts for increasing/decreasing text size

--- lisp/ekeys.el
+++ lisp/ekeys.el
@@ -5,6 +5,9 @@
 (global-set-key (kbd "<f11>") 'revert-buffer)
 (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
 (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+
 (global-set-key (kbd "M-/") 'delete-trailing-whitespace)
 (global-set-key (kbd "C-<") 'delete-backward-char)
 
