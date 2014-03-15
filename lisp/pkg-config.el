c281fb90d27a7021d88b55c3f351bd670ec1a05f
Trevor Abell <trevor@keikocode.com>
2014-03-14 19:12:37 -0700

    spacing fix

--- lisp/pkg-config.el
+++ lisp/pkg-config.el
@@ -1,34 +1,34 @@
 (add-hook 'scheme-mode-hook 'paredit-mode)
 
 (add-hook 'hs-minor-mode-hook
-					(lambda ()   (global-set-key (kbd "C-x C-o") 'hs-toggle-hiding)))
+          (lambda ()   (global-set-key (kbd "C-x C-o") 'hs-toggle-hiding)))
 
 (add-hook 'python-mode-hook
-					(lambda () 
-						(hs-minor-mode 1)
-						(fset 'hide-next
-									"\C-e\C-x\C-o\C-n")))
+          (lambda () 
+            (hs-minor-mode 1)
+            (fset 'hide-next
+                  "\C-e\C-x\C-o\C-n")))
 
 (when (require 'flymake-google-cpplint nil t)
 ;;; remember to install google-lint.py
-	(progn
-		(defun my:flymake-google-init ()
-			(require 'flymake-google-cpplint)
-			(custom-set-variables
-			 '(flymake-google-cpplint-command "/usr/bin/cpplint"))
-			(flymake-google-cpplint-load))
-		(add-hook 'c++-mode-hook 'my:flymake-google-init)
-		(add-hook 'c-mode-hook 'my:flymake-google-init)))
+  (progn
+    (defun my:flymake-google-init ()
+      (require 'flymake-google-cpplint)
+      (custom-set-variables
+       '(flymake-google-cpplint-command "/usr/bin/cpplint"))
+      (flymake-google-cpplint-load))
+    (add-hook 'c++-mode-hook 'my:flymake-google-init)
+    (add-hook 'c-mode-hook 'my:flymake-google-init)))
 
 (defun my:ac-c-header-init ()
-	(require 'auto-complete-c-headers)
-	(add-to-list 'ac-sources 'ac-source-c-headers)
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers)
 (add-to-list 'achead:include-directories '"/usr/lib/gcc/x86_64-pc-linux-gnu/4.7.3/include/g++-v4"))
 
 (add-hook 'c++-mode-hook
-					(lambda ()
-						(semantic-mode)
-						(my:ac-c-header-init)))
+          (lambda ()
+            (semantic-mode)
+            (my:ac-c-header-init)))
 
 ;; configure packages
 (when (require 'color-theme nil t)
@@ -36,51 +36,51 @@
   (color-theme-dark-blue2))
 
 (when (require 'auto-complete nil t)
-	(progn
-		(require 'auto-complete-config)
-		(ac-config-default)
-		(add-hook 'c-mode-common-hook (lambda ()
-																	(auto-complete-mode 1)))
-		(add-hook 'emacs-lisp-mode-hook (lambda ()
-																	(auto-complete-mode 1)
-																	(show-paren-mode)))
+  (progn
+    (require 'auto-complete-config)
+    (ac-config-default)
+    (add-hook 'c-mode-common-hook (lambda ()
+                                  (auto-complete-mode 1)))
+    (add-hook 'emacs-lisp-mode-hook (lambda ()
+                                  (auto-complete-mode 1)
+                                  (show-paren-mode)))
 
-		(add-hook 'python-mode-hook (lambda ()
-																	(auto-complete-mode 1)))))
+    (add-hook 'python-mode-hook (lambda ()
+                                  (auto-complete-mode 1)))))
 
 (when (require 'paredit nil t)
-	(add-hook 'emacs-lisp-mode-hook (lambda ()
-																		(paredit-mode))))
+  (add-hook 'emacs-lisp-mode-hook (lambda ()
+                                    (paredit-mode))))
 
 (when (require 'powershell-mode nil t)
-	(add-to-list 'auto-mode-alist '("\\.ps1" . powershell-mode)))
+  (add-to-list 'auto-mode-alist '("\\.ps1" . powershell-mode)))
 
 (when (and (require 'e2wm nil t)
-					 (require 'edbi nil t))
-	(load-library "e2wm-edbi"))
+           (require 'edbi nil t))
+  (load-library "e2wm-edbi"))
 
 (when (require 'yasnippet nil t)
-	(setq snippet-dirs 
-				`(,(concat emacs-root "emacs/data/snippets/")))
-	(yas/global-mode 1))
+  (setq snippet-dirs 
+        `(,(concat emacs-root "emacs/data/snippets/")))
+  (yas/global-mode 1))
 
 (when (require 'pymacs nil t)
-	(setenv "PYMACS_PYTHON" "python2")
-	(pymacs-load "ropemacs" "rope-" t))
+  (setenv "PYMACS_PYTHON" "python2")
+  (pymacs-load "ropemacs" "rope-" t))
 
 (when (require 'nose nil t)
-	(defalias 'no 'nosetests-one)
-	(defalias 'na 'nosetests-all)
-	(defalias 'np 'nosetests-pdb-one)
-	(defalias 'nm 'nosetests-module))
+  (defalias 'no 'nosetests-one)
+  (defalias 'na 'nosetests-all)
+  (defalias 'np 'nosetests-pdb-one)
+  (defalias 'nm 'nosetests-module))
 
 (when (require 'virtualenv nil t))
 
 (when (require 'csharp-mode nil t)
-	(add-hook 'csharp-mode-hook 'auto-revert-mode))
+  (add-hook 'csharp-mode-hook 'auto-revert-mode))
 
 (when (require 'hidden-mode-line-mode nil t)
-	(add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode))
+  (add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode))
 
 (when (require 'google-c-style nil t)
   (add-hook 'c-mode-commonhook 'google-c-style)
