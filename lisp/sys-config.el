d7b6c76ca184a7078bd98b9021df4fee259c051a
Trevor Abell <trevor@keikocode.com>
2014-03-14 19:11:51 -0700

    added suppression of backup files

--- lisp/sys-config.el
+++ lisp/sys-config.el
@@ -1,23 +1,23 @@
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 (menu-bar-mode 0)
-
+(setq make-backup-files nil)
 (setq initial-scratch-message "")
 
 (if (or (eq system-type 'windows-nt)
-				(eq system-type 'cygwin))
+        (eq system-type 'cygwin))
     (progn
       (defun open-explorer ()
-				(interactive)
-				(shell-command "explorer ."))
+        (interactive)
+        (shell-command "explorer ."))
       (global-set-key (kbd "<f12>") 'open-explorer)
-			(set-face-attribute 'default nil :font "Consolas 8"))
+      (set-face-attribute 'default nil :font "Consolas 8"))
   nil)
 
 
 (if (or (eq system-type 'gnu/linux))
-		(add-to-list 'default-frame-alist
-			     '(font . "Inconsolata 9")))
+    (add-to-list 'default-frame-alist
+                 '(font . "Inconsolata 9")))
 
 (add-to-list 'auto-mode-alist '(".emacs.loadpackages" . emacs-lisp-mode))
 
@@ -36,4 +36,4 @@
 
 (require 'server)
 (unless (server-running-p)
-		(server-start))
+  (server-start))
