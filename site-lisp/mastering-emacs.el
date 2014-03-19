9728bd474a6d0fe5c729912a54e958533fdee6f5
Trevor Abell <trevor@keikocode.com>
2014-03-18 18:16:18 -0700

    Added commands from mastering-emacs http://www.masteringemacs.org/articles/2010/12/22/fixing-mark-commands-transient-mark-mode/

---
+++ site-lisp/mastering-emacs.el
@@ -0,0 +1,14 @@
+(defun push-mark-no-activate ()
+  "Pushes `point' to `mark-ring' and does not activate the region
+Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
+  (interactive)
+  (push-mark (point) t nil)
+  (message "Pushed mark to ring"))
+(global-set-key (kbd "C-,") 'push-mark-no-activate)
+
+(defun jump-to-mark ()
+  "Jumps to the local mark, respecting the `mark-ring' order.
+This is the same as using \\[set-mark-command] with the prefix argument."
+  (interactive)
+  (set-mark-command 1))
+(global-set-key (kbd "M-`") 'jump-to-mark)
