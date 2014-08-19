0bd927e88a5bb42f7371ab631d7efbd8e0f17519
Trevor Abell <trevor@keikocode.com>
2014-08-18 20:49:53 -0700

    added some autocomplete for ielm

--- site-lisp/mastering-emacs.el
+++ site-lisp/mastering-emacs.el
@@ -4,6 +4,7 @@ Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
   (interactive)
   (push-mark (point) t nil)
   (message "Pushed mark to ring"))
+
 (global-set-key (kbd "C-,") 'push-mark-no-activate)
 
 (defun jump-to-mark ()
@@ -11,10 +12,24 @@ Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
 This is the same as using \\[set-mark-command] with the prefix argument."
   (interactive)
   (set-mark-command 1))
+
 (global-set-key (kbd "M-`") 'jump-to-mark)
  
 
 (defun kill-this-buffer () 
   (interactive) 
   (kill-buffer (current-buffer)))
+
 (global-set-key (kbd "C-x C-k") 'kill-this-buffer)
+
+(defun ielm-auto-complete ()
+  "Enables `auto-complete' support in \\[ielm]."
+  (setq ac-sources '(ac-source-functions
+                     ac-source-variables
+                     ac-source-features
+                     ac-source-symbols
+                     ac-source-words-in-same-mode-buffers))
+  (add-to-list 'ac-modes 'inferior-emacs-lisp-mode)
+  (auto-complete-mode 1))
+
+(add-hook 'ielm-mode-hook 'ielm-auto-complete)
