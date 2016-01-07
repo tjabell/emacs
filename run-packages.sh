d62820de6de49592e831cd6936cf1a5f57e69108
Trevor Abell <trevor@keikocode.com>
2016-01-07 07:45:45 -0700

    fixed script to be runnable from anywhere

--- run-packages.sh
+++ run-packages.sh
@@ -1,1 +1,5 @@
-emacs --batch -l packages.el
+#!/usr/bin/env bash
+
+# Get the current directory that this script is executing inq
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+emacs --batch -l $DIR/packages.el
