65a203d28b3d3ea61843bf3d372b37bf1e4ce0b4
Trevor Abell <trevor.abell@parsus.com>
2016-06-15 16:00:49 -0700

    Add macro to work on region or return string

--- lisp/csharp-workflow.el
+++ lisp/csharp-workflow.el
@@ -3,54 +3,89 @@
 
 (defalias 'clt 'copy-last-test)
 
-(defun tabell/make-mocks (ξstring &optional ξfrom ξto)
-  "Transform from a declaration in the class to a set of mocks
+(defmacro definteractive-string-func (functionname stringfunc)
+  "Creates a function which operates on a region or returns a string"
+  (let ((funsymbol (intern (concat "tabell-" functionname))))
+    `(defun ,funsymbol (ξstring &optional ξfrom ξto)
+       (interactive
+        (if (use-region-p)
+            (list nil (region-beginning) (region-end))
+          (let ((bds (bounds-of-thing-at-point 'paragraph)) )
+            (list nil (car bds) (cdr bds)) ) ) )
 
-When called interactively, work on current paragraph or text selection.
+       (let (workOnStringP inputStr outputStr)
+         (setq workOnStringP (if ξstring t nil))
+         (setq inputStr (if workOnStringP ξstring (buffer-substring-no-properties ξfrom ξto)))
+         (setq outputStr
+               (funcall ,stringfunc inputStr))
+         (if workOnStringP
+             outputStr
+           (save-excursion
+             (delete-region ξfrom ξto)
+             (goto-char ξfrom)
+             (insert outputStr) )) )
+         )))
 
-When called in lisp code, if ξstring is non-nil, returns a changed string.
-If ξstring nil, change the text in the region between positions ξfrom ξto."
-  (interactive
-   (if (use-region-p)
-       (list nil (region-beginning) (region-end))
-     (let ((bds (bounds-of-thing-at-point 'paragraph)) )
-       (list nil (car bds) (cdr bds)) ) ) )
 
-  (let (workOnStringP inputStr outputStr)
-    (setq workOnStringP (if ξstring t nil))
-    (setq inputStr (if workOnStringP ξstring (buffer-substring-no-properties ξfrom ξto)))
-    (setq outputStr
-          (let ((case-fold-search t))
-            (replace-regexp-in-string  "private readonly \\(.*\\) \\(.*\\);" "\\2 = Mock<\\1>();"  inputStr) )  )
-    (if workOnStringP
-        outputStr
-      (save-excursion
-        (delete-region ξfrom ξto)
-        (goto-char ξfrom)
-        (insert outputStr) )) ) )
+(definteractive-string-func "make-mocks" (lambda (region-or-buffer)
+                                           (let ((case-fold-search t))
+                                             (replace-regexp-in-string  "private readonly \\(.*\\) \\(.*\\);" "\\2 = Mock<\\1>();"  region-or-buffer) )
+                                           ))
 
-(defun tabell/make-mocks-parameters (ξstring &optional ξfrom ξto)
-  "Transform from a declaration in the class to a set of mocks
+(definteractive-string-func "make-mock-parameters" (lambda (region-or-buffer)
+                                                     
+                                                     (let ((case-fold-search t))
+                                                       (replace-regexp-in-string  "[[:space:]]*\\(.*\\) =.*" "\\1,\n"  region-or-buffer) )
+                                           ))
 
-When called interactively, work on current paragraph or text selection.
+;; (defun tabell/make-mocks (ξstring &optional ξfrom ξto)
+;;   "Transform from a declaration in the class to a set of mocks
 
-When called in lisp code, if ξstring is non-nil, returns a changed string.
-If ξstring nil, change the text in the region between positions ξfrom ξto."
-  (interactive
-   (if (use-region-p)
-       (list nil (region-beginning) (region-end))
-     (let ((bds (bounds-of-thing-at-point 'paragraph)) )
-       (list nil (car bds) (cdr bds)) ) ) )
+;; When called interactively, work on current paragraph or text selection.
 
-  (let (workOnStringP inputStr outputStr)
-    (setq workOnStringP (if ξstring t nil))
-    (setq inputStr (if workOnStringP ξstring (buffer-substring-no-properties ξfrom ξto)))
-    (setq outputStr
-          (let ((case-fold-search t))
-            (replace-regexp-in-string  "[[:space:]]*\\(.*\\) =.*" "\\1,\n"  inputStr) )  )
-    (if workOnStringP
-        outputStr
-      (save-excursion
-        (delete-region ξfrom ξto)
-        (goto-char ξfrom)
-        (insert outputStr) )) ) )
+;; When called in lisp code, if ξstring is non-nil, returns a changed string.
+;; If ξstring nil, change the text in the region between positions ξfrom ξto."
+;;   (interactive
+;;    (if (use-region-p)
+;;        (list nil (region-beginning) (region-end))
+;;      (let ((bds (bounds-of-thing-at-point 'paragraph)) )
+;;        (list nil (car bds) (cdr bds)) ) ) )
+
+;;   (let (workOnStringP inputStr outputStr)
+;;     (setq workOnStringP (if ξstring t nil))
+;;     (setq inputStr (if workOnStringP ξstring (buffer-substring-no-properties ξfrom ξto)))
+;;     (setq outputStr
+;;           (let ((case-fold-search t))
+;;             (replace-regexp-in-string  "private readonly \\(.*\\) \\(.*\\);" "\\2 = Mock<\\1>();"  inputStr) )  )
+;;     (if workOnStringP
+;;         outputStr
+;;       (save-excursion
+;;         (delete-region ξfrom ξto)
+;;         (goto-char ξfrom)
+;;         (insert outputStr) )) ) )
+
+;; (defun tabell/make-mocks-parameters (ξstring &optional ξfrom ξto)
+;;   "Transform from a declaration in the class to a set of mocks
+
+;; When called interactively, work on current paragraph or text selection.
+
+;; When called in lisp code, if ξstring is non-nil, returns a changed string.
+;; If ξstring nil, change the text in the region between positions ξfrom ξto."
+;;   (interactive
+;;    (if (use-region-p)
+;;        (list nil (region-beginning) (region-end))
+;;      (let ((bds (bounds-of-thing-at-point 'paragraph)) )
+;;        (list nil (car bds) (cdr bds)) ) ) )
+
+;;   (let (workOnStringP inputStr outputStr)
+;;     (setq workOnStringP (if ξstring t nil))
+;;     (setq inputStr (if workOnStringP ξstring (buffer-substring-no-properties ξfrom ξto)))
+;;     (setq outputStr
+;;           (let ((case-fold-search t))
+;;             (replace-regexp-in-string  "[[:space:]]*\\(.*\\) =.*" "\\1,\n"  inputStr) )  )
+;;     (if workOnStringP
+;;         outputStr
+;;       (save-excursion
+;;         (delete-region ξfrom ξto)
+;;         (goto-char ξfrom)
+;;         (insert outputStr) )) ) )
