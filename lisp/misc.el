ed384e0acedafb07dd464e929a805fd61a796505
Trevor Abell <trevor.abell@parsus.com>
2017-08-07 21:32:27 -0700

    lisp/misc - added amazon pricing function

--- lisp/misc.el
+++ lisp/misc.el
@@ -1,2 +1,7 @@
 (defun insert-current-date () (interactive)
        (insert (shell-command-to-string "echo -n $(date +%Y-%m-%d)")))
+
+(defun price-amazon (n)
+  (interactive "nEnter amount per hour: ")
+  (with-current-buffer (current-buffer)
+    (insert "Hour - " (number-to-string n) " Month - " (number-to-string  (* n 730)) " Year - " (number-to-string (* n 730 12)))))
