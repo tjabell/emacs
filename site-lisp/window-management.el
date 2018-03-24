4d9b89250f7d30fc7e5ce7521f6f463f72683b29
Trevor Abell <trevor.abell@parsus.com>
2018-03-23 17:55:42 -0700

    Make package name more descriptive

---
+++ site-lisp/window-management.el
@@ -0,0 +1,49 @@
+;;; From https://www.emacswiki.org/emacs/TransposeWindows
+;;; Robert bost
+(defun rotate-windows (arg)
+  "Rotate your windows; use the prefix argument to rotate the other direction"
+  (interactive "P")
+  (if (not (> (count-windows) 1))
+      (message "You can't rotate a single window!")
+    (let* ((rotate-times (prefix-numeric-value arg))
+           (direction (if (or (< rotate-times 0) (equal arg '(4)))
+                          'reverse 'identity)))
+      (dotimes (_ (abs rotate-times))
+        (dotimes (i (- (count-windows) 1))
+          (let* ((w1 (elt (funcall direction (window-list)) i))
+                 (w2 (elt (funcall direction (window-list)) (+ i 1)))
+                 (b1 (window-buffer w1))
+                 (b2 (window-buffer w2))
+                 (s1 (window-start w1))
+                 (s2 (window-start w2))
+                 (p1 (window-point w1))
+                 (p2 (window-point w2)))
+            (set-window-buffer-start-and-point w1 b2 s2 p2)
+            (set-window-buffer-start-and-point w2 b1 s1 p1)))))))
+
+;;; Steve Yegge
+;; someday might want to rotate windows if more than 2 of them
+(defun swap-windows ()
+ "If you have 2 windows, it swaps them." (interactive) (cond ((not (= (count-windows) 2)) (message "You need exactly 2 windows to do this."))
+ (t
+ (let* ((w1 (first (window-list)))
+	 (w2 (second (window-list)))
+	 (b1 (window-buffer w1))
+	 (b2 (window-buffer w2))
+	 (s1 (window-start w1))
+	 (s2 (window-start w2)))
+ (set-window-buffer w1 b2)
+ (set-window-buffer w2 b1)
+ (set-window-start w1 s2)
+ (set-window-start w2 s1)))))
+
+(defun rename-file-and-buffer (new-name)
+	"Renames both current buffer and file it's visiting to NEW-NAME." (interactive "sNew name: ")
+	(let ((name (buffer-name))
+				(filename (buffer-file-name)))
+		(if (not filename)
+				(message "Buffer '%s' is not visiting a file!" name)
+			(if (get-buffer new-name)
+					(message "A buffer named '%s' already exists!" new-name)
+				(progn 	 (rename-file name new-name 1) 	 (rename-buffer new-name) 	 (set-visited-file-name new-name) 	 (set-buffer-modified-p nil))))))
+
