a343c87d75660ba07cda95d46004141a57cc07ac
Trevor Abell <trevor.abell@parsus.com>
2018-11-06 08:52:51 -0700

    Added a funciton to print first character of a line, used for katakana flashcards

--- lisp/misc.el
+++ lisp/misc.el
@@ -5,3 +5,9 @@
   (interactive "nEnter amount per hour: ")
   (with-current-buffer (current-buffer)
     (insert "Hour - " (number-to-string n) " Month - " (number-to-string  (* n 730)) " Year - " (number-to-string (* n 730 12)))))
+
+(defun first-char-printer ()
+  "Used this to print out katakana characters for anki slide deck"
+  (interactive)
+  (let ((c (string-to-char (buffer-substring (line-beginning-position) (1+ (line-beginning-position))))))
+    (prin1 (get-char-code-property  c 'name) (current-buffer))))
