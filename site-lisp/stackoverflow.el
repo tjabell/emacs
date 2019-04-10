36118d06d1067627d7cdeae90bb3bcdcd89478b4
Trevor Abell <trevor.abell@parsus.com>
2019-04-10 13:40:54 -0700

    Add my-capitalize-all-sqlserver-keywords function

--- site-lisp/stackoverflow.el
+++ site-lisp/stackoverflow.el
@@ -7,3 +7,20 @@
   ;; effects of backward-sexp and forward-sexp.
   (backward-sexp (1+ arg))
   (forward-sexp 1))
+
+;;; https://stackoverflow.com/questions/22091936/emacs-how-to-capitalize-all-keywords-example-in-sql
+(defun point-in-comment ()
+  (let ((syn (syntax-ppss)))
+    (and (nth 8 syn)
+         (not (nth 3 syn)))))
+
+(defun my-capitalize-all-sqlserver-keywords ()
+  (interactive)
+  (require 'sql)
+  (save-excursion
+    (dolist (keywords sql-mode-ms-font-lock-keywords) 
+      (goto-char (point-min))
+      (while (re-search-forward (car keywords) nil t)
+        (unless (point-in-comment)
+          (goto-char (match-beginning 0))
+          (upcase-word 1))))))
