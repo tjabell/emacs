5b2895da7eb3c4ad3c01326a2a824387ad1f75fe
Trevor Abell <trevor.abell@parsus.com>
2019-04-29 07:00:13 -0700

    add shell/term shortcuts to f2

--- lisp/ekeys.el
+++ lisp/ekeys.el
@@ -34,7 +34,6 @@
 ;;; Function keys
 (global-set-key (kbd "C-!") 'eshell-here)
 
-(global-set-key (kbd "<f1> s") 'shell)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
 (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
 (global-set-key (kbd "<f1> l") 'counsel-find-library)
@@ -42,6 +41,8 @@
 
 (define-prefix-command 'f2-map)
 (global-set-key (kbd "<f2>") 'f2-map)
+(global-set-key (kbd "<f2> h") 'shell)
+(global-set-key (kbd "<f2> t") 'term)
 (global-set-key (kbd "<f2> s") '(lambda () (interactive) (switch-to-buffer "*scratch*")))
 (global-set-key (kbd "<f2> n") '(lambda () (interactive) (find-file "~/org/notes.org")))
 (global-set-key (kbd "<f2> o") '(lambda () (interactive) (find-file "~/org/emacs-notes.org")))
