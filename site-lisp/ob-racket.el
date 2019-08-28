2b65d7a2e608f5add720cb423aaaabb5c2dc5d6a
Trevor Abell <trevor.abell@parsus.com>
2019-08-28 11:30:32 +0700

    ob-racket, org-shell snippet, org-racket snippet

--- site-lisp/ob-racket.el
+++ site-lisp/ob-racket.el
@@ -1,85 +1,91 @@
-;; ob-racket.el
-;; Chris Vig (chris@invictus.so)
+;;; ob-racket.el --- org-babel functions for racket evaluation
 
-;; -- Provide --
+;; Copyright (C) 2015 Waldemar Quevedo
+;; Author: Waldemar Quevedo, based on ob-ruby and ob-java by Eric Schulte
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Org Babel support for evaluating Racket source code.
+;;
+;; Support is very basic, passing variables from code blocks
+;; is still not supported for example
+
+;;; Code:
+
+(require 'ob)
+(eval-when-compile (require 'cl))
+
+(defvar org-babel-tangle-lang-exts)
+(add-to-list 'org-babel-tangle-lang-exts '("racket" . "rkt"))
+
+(defvar org-babel-default-header-args:racket '())
+
+(defvar org-babel-racket-command "racket"
+  "Name of command to use for executing racket code.")
+
+(defcustom org-babel-racket-hline-to "nil"
+  "Replace hlines in incoming tables with this when translating to racket."
+  :group 'org-babel
+  :version "24.4"
+  :package-version '(Org . "8.0")
+  :type 'string)
+
+(defcustom org-babel-racket-nil-to 'hline
+  "Replace 'nil' in racket tables with this before returning."
+  :group 'org-babel
+  :version "24.4"
+  :package-version '(Org . "8.0")
+  :type 'symbol)
+
+(defun org-babel-execute:racket (body params)
+  "Execute a block of Racket code with Babel.
+This function is called by `org-babel-execute-src-block'."
+  (let* ((result-params (cdr (assoc :result-params params)))
+         (result-type   (cdr (assoc :result-type params)))
+         (racket-lang   (cdr (assoc :lang params)))
+         (full-body (if (> (length racket-lang) 0)
+                        (concat "#lang " racket-lang "\n\n" body)
+                      body))
+         (src-file (org-babel-temp-file "racket-"))
+         (result (progn (with-temp-file src-file (insert full-body))
+                        (org-babel-eval
+                         (concat org-babel-racket-command " " src-file) ""))))
+
+    (org-babel-reassemble-table
+     (org-babel-result-cond result-params
+       result
+       (org-babel-racket-table-or-string result))
+     (org-babel-pick-name (cdr (assoc :colname-names params))
+                          (cdr (assoc :colnames params)))
+     (org-babel-pick-name (cdr (assoc :rowname-names params))
+                          (cdr (assoc :rownames params))))))
+
+(defun org-babel-racket-table-or-string (results)
+  "Convert RESULTS into an appropriate elisp value.
+If RESULTS look like a table, then convert them into an
+Emacs-lisp table, otherwise return the results as a string."
+  (let ((res (org-babel-script-escape results)))
+    (if (listp res)
+        (mapcar (lambda (el) (if (equal el 'nil)
+                            org-babel-racket-nil-to el))
+                res)
+      res)))
 
 (provide 'ob-racket)
 
-;; -- Requires --
-
-(require 'ob)
-
-;; -- Variables --
-
-(add-to-list 'org-babel-tangle-lang-exts '("racket" . "rkt"))
-
-(defvar org-babel-default-header-args:racket '((:lang . "racket"))
-  "A list of default header args for Racket code blocks.")
-
-(defvar org-babel-command:racket "/usr/bin/racket"
-  "The path to the Racket interpreter executable.")
-
-;; -- Babel Functions --
-
-(defun org-babel-expand-body:racket (body params)
-  "Expands the body of a Racket code block."
-  ;; Currently we don't do any expansion for tangled blocks. Just return
-  ;; body unmodified as specified by the user.
-  body)
-
-(defun org-babel-execute:racket (body params)
-  "Executes a Racket code block."
-  ;; Round up the stuff we need
-  (let* ((parsed-params (ob-racket--parse-params params))
-	 (expanded-body (org-babel-expand-body:racket body params))
-	 (result-type (nth 0 parsed-params))
-	 (lang (nth 1 parsed-params))
-	 (vars (nth 2 parsed-params))
-	 (temp-file (make-temp-file "ob-racket-")))
-    ;; Build script in temporary file
-    (with-temp-file temp-file
-      (cond
-       ;; Results type is "value" - run in let form
-       ((equal result-type 'value)
-	(let ((vars-string
-	      (mapconcat (lambda (var) (format "[%s (quote %s)]" (car var) (cdr var))) vars " ")))
-	  (insert (format "#lang %s\n(let (%s)\n%s)"
-			  lang
-			  vars-string
-			  expanded-body))))
-       ;; Results type is "output" - run as script
-       ((equal result-type 'output)
-	(let ((vars-string
-	       (mapconcat (lambda (var) (format "(define %s (quote %s))" (car var) (cdr var))) vars "\n")))
-	  (insert (format "#lang %s\n%s\n%s"
-			  lang
-			  vars-string
-			  body))))
-       ;; Unknown result type??
-       (t (error "Invalid result type: %s" result-type))))
-    ;; Run script with Racket interpreter, delete temp file, and return output
-    (with-temp-buffer
-      (prog2
-	  (call-process org-babel-command:racket nil (current-buffer) nil temp-file)
-	  (buffer-string)
-	(delete-file temp-file)))))
-
-(defun org-babel-prep-session:racket (session params)
-  (error "Racket does not currently support sessions."))
-
-;; -- Parameter Parsing --
-
-(defun ob-racket--parse-params (params)
-  "Processes and parses parameters for an Org Babel code block. The results are
-returned as a list."
-  (let ((processed-params (org-babel-process-params params))
-	(result-type nil)
-	(lang nil)
-	(vars nil))
-    (dolist (processed-param processed-params)
-      (let ((key (car processed-param)) (value (cdr processed-param)))
-	(cond
-	 ((equal key :result-type) (setq result-type value))
-	 ((equal key :lang) (setq lang value))
-	 ((equal key :var) (push value vars)))))
-    (list result-type lang vars)))
+;;; ob-racket.el ends here
