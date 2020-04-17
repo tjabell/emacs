4bfb3f1c5bdda6a19403ccf6422b242188b7160e
Trevor Abell <trevor.abell@parsus.com>
2020-04-17 11:38:44 +0800

    Add insert code signature fn

--- lisp/my-misc.el
+++ lisp/my-misc.el
@@ -1,6 +1,9 @@
 (defun insert-current-date () (interactive)
        (insert (shell-command-to-string "echo -n $(date +%Y-%m-%d)")))
 
+(defun insert-signature-for-code () (interactive)
+       (insert (shell-command-to-string "echo -n $(date +%Y%m%d) -tja")))
+
 (defun price-amazon (n)
   (interactive "nEnter amount per hour: ")
   (with-current-buffer (current-buffer)
