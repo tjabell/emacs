706672940ca788e1f57875fbb00b18dd5123b676
Trevor Abell <trevor.abell@parsus.com>
2020-05-11 09:03:16 +0700

    my-clockify:add acdhh ids

--- lisp/my-clockify.el
+++ lisp/my-clockify.el
@@ -1,4 +1,4 @@
-(defun my:clockify-init () 
+(defun my:clockify-init ()
 ;;; Need this to initialize clockify api I guess
   (setq my:clockify:userid  (clockify--user-info))
 
@@ -13,7 +13,13 @@
   (setq my:gilbert:project-id "5e948870ea8094116e8c7188")
 
   (setq my:karisma-pgs:project-id "5d702211a3fe2c1b233e9e30")
-  
+
   (setq my:bonappetit-hosting-project-id "5ea87fcd7468d5567e641f34")
 
-  (setq my:parsus-training-id "5d702b48a3fe2c1b233ea83c"))
+  (setq my:parsus-training-id "5d702b48a3fe2c1b233ea83c")
+
+  (setq my:acdhh-project-id "5d6fff262b576659a087efb4")
+
+  (setq my:acdhh-public-website-task-id "5d6fffd2a3fe2c1b233e709b")
+
+  (setq my:acdhh-cdbms-task-id "5d6fffd72b576659a087f096"))
