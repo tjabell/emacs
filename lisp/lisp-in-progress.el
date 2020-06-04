60c46d8f9e9ddcb90adf0ac6aad8b0bbe72476f0
Trevor Abell <trevor.abell@parsus.com>
2020-06-04 18:25:32 +0700

    Add tempo-snippets

--- lisp/lisp-in-progress.el
+++ lisp/lisp-in-progress.el
@@ -1,2 +1,6 @@
 (tempo-define-template "assembla-ticket-email"
                        '((p "Space: " project) "+" p "@tickets.assembla.com"))
+
+
+(tempo-define-snippet "assembla-ticket-email"
+                       '((p "Space: " project) "+" p "@tickets.assembla.com"))
