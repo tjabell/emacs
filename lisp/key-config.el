a11f3929783ab12281f26858bd1a23a14de12660
Trevor Abell <trevor.abell@parsus.com>
2021-08-01 16:38:13 -0500

    remove C-c o for emacs-notes file

--- lisp/key-config.el
+++ lisp/key-config.el
@@ -68,7 +68,6 @@
 (global-set-key (kbd "C-<tab>") 'next-buffer)
 (global-set-key (kbd "C-<iso-lefttab>") 'previous-buffer)
 
-(global-set-key (kbd "C-c o") '(lambda () (interactive) (find-file "~/org/emacs-notes.org")))
 (global-set-key (kbd "C-c p") '(lambda () (interactive) (find-file "~/projects/projects.org")))
 
 (global-set-key (kbd "M-p") 'ace-window)
