64c90a599c88f751a5e86d958c8ca1eb531fbbe9
Trevor Abell <trevor.abell@parsus.com>
2021-08-01 16:39:06 -0500

    gsi shortcuts

--- lisp/my-projects.el
+++ lisp/my-projects.el
@@ -7,3 +7,11 @@
   (interactive)
   (rename-buffer "*vterm* Franchisee Portal Angular Client"))
 
+
+(defun my:gsi:rename-fp-api-vterm-buffer ()
+  (interactive)
+  (rename-buffer "*vterm* Franchisee Portal API"))
+(defun my:gsi:rename-faculty-api-vterm-buffer ()
+  (interactive)
+  (rename-buffer "*vterm* Faculty API"))
+
