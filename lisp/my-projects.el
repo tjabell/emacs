d32fcb4fd60c12c0ffbc65279bf2373b90fdfd63
Trevor Abell <trevor.abell@parsus.com>
2021-08-25 15:27:20 -0500

    update my:gsi rename buffer func name

--- lisp/my-projects.el
+++ lisp/my-projects.el
@@ -1,16 +1,16 @@
-
+;;; GSI Project functions
 (defun my:gsi:rename-aem-vterm-buffer ()
   (interactive)
   (rename-buffer "*vterm* AEM Author Instance"))
 
-(defun my:gsi:rename-fp-vterm-buffer ()
+(defun my:gsi:rename-fp-web-vterm-buffer ()
   (interactive)
   (rename-buffer "*vterm* Franchisee Portal Angular Client"))
-
 
 (defun my:gsi:rename-fp-api-vterm-buffer ()
   (interactive)
   (rename-buffer "*vterm* Franchisee Portal API"))
+
 (defun my:gsi:rename-faculty-api-vterm-buffer ()
   (interactive)
   (rename-buffer "*vterm* Faculty API"))
