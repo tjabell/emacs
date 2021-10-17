88db4bc621f4a67de8c601834c9afa06ef49975b
Trevor Abell <trevor.abell@parsus.com>
2021-10-17 12:40:39 -0500

    ivy-rich, helpful

--- lisp/pkg-config.el
+++ lisp/pkg-config.el
@@ -458,3 +458,17 @@
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
