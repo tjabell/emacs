fbea4f9c91c56788a8fef7ed46fa978d7e81c4db
Trevor Abell <trevor.abell@parsus.com>
2021-10-17 12:27:44 -0500

    projects/create some more gsi rename fns

--- lisp/my-projects.el
+++ lisp/my-projects.el
@@ -3,6 +3,10 @@
   (interactive)
   (rename-buffer "*vterm* AEM Author Instance"))
 
+(defun my:gsi:rename-content-api-vterm-buffer ()
+  (interactive)
+  (rename-buffer "*vterm* Content API"))
+
 (defun my:gsi:rename-fp-web-vterm-buffer ()
   (interactive)
   (rename-buffer "*vterm* Franchisee Portal Angular Client"))
@@ -14,4 +18,12 @@
 (defun my:gsi:rename-faculty-api-vterm-buffer ()
   (interactive)
   (rename-buffer "*vterm* Faculty API"))
+
+;;; WIP
+;;; https://github.com/akermu/emacs-libvterm/issues/55
+(defun my:gsi:vterm-rename-sensibly ()
+  (interactive)
+  (let ((folder ()))
+    (rename-buffer "*vterm* " folder)))
+
 
