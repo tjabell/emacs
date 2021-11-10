1fa308ccbc0823a86a4072657e4ed2e105d5d013
tjabell@gmail.com <trevor.abell@parsus.com>
2021-11-09 18:09:03 -0700

    move org-config to new config

--- lisp/key-config.el
+++ lisp/key-config.el
@@ -15,12 +15,6 @@
 (global-set-key (kbd "M-/") 'delete-trailing-whitespace)
 (global-set-key (kbd "C-c C-f") 'iy-go-to-char)
 
-;;; org-mode activate
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c b") 'org-iswitchb)
-
 ;;; Super + uppercase letter signifies a buffer/file
 (global-set-key (kbd "s-S")                       ;; scratch
                 (lambda()(interactive)(switch-to-buffer "*scratch*")))
