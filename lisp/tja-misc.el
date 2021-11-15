63072bfc1b25860ae43e55fe2b97b68d4749a6da
tjabell@gmail.com <trevor@trevor-laptop-x1.local>
2021-11-14 20:09:39 -0700

    add tja-misc

--- lisp/tja-misc.el
+++ lisp/tja-misc.el
@@ -1,24 +1,23 @@
+;;;###autoload
 (defun insert-current-date ()
   (interactive)
   (insert (shell-command-to-string "echo -n $(date +%Y-%m-%d)")))
 (defalias 'icd 'insert-current-date)
 
+;;;###autoload
 (defun insert-signature-for-code ()
   (interactive)
   (insert (shell-command-to-string "echo -n $(date +%Y%m%d)TJA")))
 (defalias 'isc 'insert-signature-for-code)
 
-(defun price-amazon (n)
-  (interactive "nEnter amount per hour: ")
-  (with-current-buffer (current-buffer)
-    (insert "Hour - " (number-to-string n) " Month - " (number-to-string  (* n 730)) " Year - " (number-to-string (* n 730 12)))))
-
+;;;###autoload
 (defun first-char-printer ()
   "Used this to print out katakana characters for anki slide deck"
   (interactive)
   (let ((c (string-to-char (buffer-substring (line-beginning-position) (1+ (line-beginning-position))))))
     (prin1 (get-char-code-property  c 'name) (current-buffer))))
 
+;;;###autoload
 (defun ebpa/edebug-remove-all-instrumentation ()
   "Remove all edebug instrumentation by visiting each function
 definition and running `eval-defun`."
@@ -30,11 +29,10 @@ definition and running `eval-defun`."
          (goto-char (marker-position pos))
          (eval-defun nil))))))
 
-
-(defun my/newline-directly-below ()
+;;;###autoload
+(defun tja-newline-directly-below ()
   "1. move to end of the line.
    2. insert newline with index"
-
   (interactive)
   (let ((oldcol (current-column)))
     (end-of-line)
@@ -43,15 +41,20 @@ definition and running `eval-defun`."
       (insert " "))))
 
 ;;; format a json string in a buffer
-(defun my:json-mode-and-format-buffer ()
+;;;###autoload
+(defun tja-json-mode-and-format-buffer ()
   (interactive)
   (json-mode)
   (json-pretty-print-buffer))
 
-(defalias 'jm 'my:json-mode-and-format-buffer)
+(defalias 'jm 'tja-json-mode-and-format-buffer)
 
 (require 'notifications)
-(defun my:remind-me-in (minutes body)
+
+;;;###autoload
+(defun tja-remind-me-in (minutes body)
        (interactive "sMinutes:\nsBody:")
        (let ((minutes (concat minutes " min")))
          (run-at-time minutes nil 'notifications-notify :title "Emacs alert" :body body)))
+
+(provide 'tja-misc)
