eddaadd425802f4b17190b72be672b36d8d47d54
Trevor Abell <tjabell@gmail.com>
2022-01-26 11:11:48 +0100

    set clocklevel to print out 4 levels by default!

--- exwm.el
+++ exwm.el
@@ -5,47 +5,50 @@
     (setq exwm-workspace-number 5))
   ;; Make class name the buffer name
   (add-hook 'exwm-update-class-hook
-	    (lambda ()
-	      (exwm-workspace-rename-buffer exwm-class-name)))
+            (lambda ()
+              (exwm-workspace-rename-buffer exwm-class-name)))
   ;; Global keybindings.
   (unless (get 'exwm-input-global-keys 'saved-value)
     (setq exwm-input-global-keys
-	  `(
-	    ;; 's-r': Reset (to line-mode).
-	    ([?\s-r] . exwm-reset)
-	    ;; 's-w': Switch workspace.
-	    ([?\s-w] . exwm-workspace-switch)
-	    ;; 's-&': Launch application.
-	    ([?\s-&] . (lambda (command)
-			 (interactive (list (read-shell-command "$ ")))
-			 (start-process-shell-command command nil command)))
-	    ;; 's-o': Other window
-	    ([?\s-o] . other-window)
-	    ;; 's-N': Switch to certain workspace.
-	    ,@(mapcar (lambda (i)
-			`(,(kbd (format "s-%d" i)) .
-			  (lambda ()
-			    (interactive)
-			    (exwm-workspace-switch-create ,i))))
-		      (number-sequence 0 9)))))
+          `(
+            ;; 's-r': Reset (to line-mode).
+            ([?\s-r] . exwm-reset)
+            ;; 's-w': Switch workspace.
+            ([?\s-w] . exwm-workspace-switch)
+            ;; 's-&': Launch application.
+            ([?\s-&] . (lambda (command)
+                         (interactive (list (read-shell-command "$ ")))
+                         (start-process-shell-command command nil command)))
+            ;; 's-o': Other window
+            ([?\s-o] . other-window)
+            ;; 's-N': Switch to certain workspace.
+            ,@(mapcar (lambda (i)
+                        `(,(kbd (format "s-%d" i)) .
+                          (lambda ()
+                            (interactive)
+                            (exwm-workspace-switch-create ,i))))
+                      (number-sequence 0 9)))))
   ;; Line-editing shortcuts
   (unless (get 'exwm-input-simulation-keys 'saved-value)
     (setq exwm-input-simulation-keys
-	  '(([?\C-b] . [left])
-	    ([?\C-f] . [right])
-	    ([?\C-p] . [up])
-	    ([?\C-n] . [down])
-	    ([?\C-a] . [home])
-	    ([?\C-e] . [end])
-	    ([?\M-v] . [prior])
-	    ([?\C-v] . [next])
-	    ([?\C-d] . [delete])
-	    ([?\C-k] . [S-end delete]))))
+          '(([?\C-b] . [left])
+            ([?\C-f] . [right])
+            ([?\C-p] . [up])
+            ([?\C-n] . [down])
+            ([?\C-a] . [home])
+            ([?\C-e] . [end])
+            ([?\M-v] . [prior])
+            ([?\C-v] . [next])
+            ([?\C-d] . [delete])
+            ([?\C-k] . [S-end delete]))))
 
   (exwm-input-set-key (kbd "s-<f12>")
-		    (lambda () (interactive) (start-process "" nil "sudo" "systemctl suspend")))
+                    (lambda () (interactive)
+                      (start-process "" nil "xss-lock" "--" "slock")
+                      (start-process "" nil "systemctl" "suspend")))
+
   (exwm-input-set-key (kbd "s-o")
-		  (lambda () (interactive) (other-window 1)))
+                  (lambda () (interactive) (other-window 1)))
 
   ;; Enable EXWM
   (exwm-enable))
